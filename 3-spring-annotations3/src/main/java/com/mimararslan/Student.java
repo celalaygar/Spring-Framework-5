69dc523d2e3406463eac4d8d208161e057bcc546
Celal Aygar <celal.aygar@gmail.com>
2018-03-26 20:02:44 +0300

    Add files via upload

---
+++ 3-spring-annotations3/src/main/java/com/mimararslan/Student.java
@@ -0,0 +1,58 @@
+package com.mimararslan;
+
+public class Student {
+	private String isim,telNo;
+	private int yas;
+	private Okul okul;
+	
+	public Student(){
+		System.out.println("Student sınıfına ait hazırlayıcı methotdur.");
+		
+	}
+
+	public Okul getOkul() {
+		return okul;
+	}
+
+	public void setOkul(Okul okul) {
+		this.okul = okul;
+	}
+
+	public String getIsim() {
+		return isim;
+	}
+
+	public void setIsim(String isim) {
+		this.isim = isim;
+	}
+
+	public String getTelNo() {
+		return telNo;
+	}
+
+	public void setTelNo(String telNo) {
+		this.telNo = telNo;
+	}
+
+
+	public int getYas() {
+		return yas;
+	}
+
+	public void setYas(int yas) {
+		this.yas = yas;
+	}
+	public void okulBilgileri(){
+		
+		System.out.println("Okul ismi : "+okul.getOkulIsmi());
+		okul.adresBilgileri();
+	}
+	
+	public void init(){
+		System.out.println("Student sınıfı init methodu.");
+	}
+	
+	public void destroy(){
+		System.out.println("Student sınıfı destroy methodu.");
+	}
+}
