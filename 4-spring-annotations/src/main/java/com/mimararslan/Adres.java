04355d62c48a1c369289fd71a43c4af839625d3e
Celal Aygar <celal.aygar@gmail.com>
2018-03-26 20:26:01 +0300

    Add files via upload

---
+++ 4-spring-annotations/src/main/java/com/mimararslan/Adres.java
@@ -0,0 +1,31 @@
+package com.mimararslan;
+
+public class Adres {
+	
+	private String il,ilce;
+	
+	
+	public Adres() {
+
+		System.out.println("Adres sınıfına ait hazırlayıcı methotdur.");
+	}
+	
+	public String getIl() {
+		return il;
+	}
+
+	public void setIl(String il) {
+		this.il = il;
+	}
+
+	public String getIlce() {
+		return ilce;
+	}
+
+	public void setIlce(String ilce) {
+		this.ilce = ilce;
+	}
+
+
+	
+}
