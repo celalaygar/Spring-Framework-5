fc0fe823d27cc222dc420dff453061a15c5b6689
Celal Aygar <celal.aygar@gmail.com>
2018-04-30 01:44:41 +0300

    Update maincontroller.java

--- 8-springmvc-FormValidation(a)/src/com/celalaygar/controller/maincontroller.java
+++ 8-springmvc-FormValidation(a)/src/com/celalaygar/controller/maincontroller.java
@@ -60,7 +60,6 @@ public class maincontroller {
 			return "PersonelForm";
 		}
 
-		theModel.addAttribute("email", personel.getEmail());
 		theModel.addAttribute("personel", personel);
 		return "PersonelSaveResult";
 	}
